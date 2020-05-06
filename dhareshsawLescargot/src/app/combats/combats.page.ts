14ac995c3e592c724ce239667187b2541a85c8d0
Couvez Jean-Christophe <jean-christophe.couvez@universite-paris-saclay.fr>
2020-05-06 17:13:57 +0200

    Creation Composant

---
+++ dhareshsawLescargot/src/app/combats/combats.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-combats',
+  templateUrl: './combats.page.html',
+  styleUrls: ['./combats.page.scss'],
+})
+export class CombatsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
