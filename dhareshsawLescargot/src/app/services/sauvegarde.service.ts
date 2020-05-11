53df294887aa225ee52cacf7f6a7b846ce6996fb
Maudet <M320267@airfrance.fr>
2020-05-11 11:40:18 +0200

    création service sauvegarde

---
+++ dhareshsawLescargot/src/app/services/sauvegarde.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SauvegardeService {
+  private heroInitial: Hero
+  //private etatduJeu: {hero: Hero, scene: Scene}
+  private histoire: {action:string , description:string}[]=[]
+
+  constructor() { }
+
+}
