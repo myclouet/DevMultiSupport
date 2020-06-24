ef4889b6dbb3d5e10b7ad37ffbac8f94232cc009
Junkeesaw <kevin555j@gmail.com>
2020-06-24 10:42:07 +0200

    maj encounter ok

--- dhareshsawLescargot/src/app/app.module.ts
+++ dhareshsawLescargot/src/app/app.module.ts
@@ -11,7 +11,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { SauvegardeService } from './services/sauvegarde.service';
 import { CharacterService } from './services/character.service';
 import { SceneService } from './services/scene.service';
-import { ObjectInventoryService } from './services/object-inventory.service';
 
 import { IonicStorageModule } from '@ionic/storage';
 
@@ -24,7 +23,6 @@ import { IonicStorageModule } from '@ionic/storage';
     SplashScreen,
     SauvegardeService,
     CharacterService,
-    ObjectInventoryService,
     SceneService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
