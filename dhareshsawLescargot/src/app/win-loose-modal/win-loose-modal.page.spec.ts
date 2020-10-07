1ce6a76026ac7b1d78524984717542cede56ce33
Kevin Junkeesaw <kevin555j@gmail.com>
2020-10-08 00:00:43 +0200

    winLooseModal test find

--- dhareshsawLescargot/src/app/win-loose-modal/win-loose-modal.page.spec.ts
+++ dhareshsawLescargot/src/app/win-loose-modal/win-loose-modal.page.spec.ts
@@ -3,6 +3,15 @@ import { IonicModule } from '@ionic/angular';
 
 import { WinLooseModalPage } from './win-loose-modal.page';
 import { NavParams } from '@ionic/angular';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+// import { TranslateService } from 'jasmine';
+import { TranslateStore } from "@ngx-translate/core/src/translate.store";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: HttpClient) {
+    return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
 
 
 describe('WinLooseModalPage', () => {
@@ -12,8 +21,16 @@ describe('WinLooseModalPage', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ WinLooseModalPage ],
-      imports: [IonicModule.forRoot()],
-      providers: [WinLooseModalPage, NavParams] 
+      imports: [IonicModule.forRoot(), HttpClientModule,
+      
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: (createTranslateLoader),
+            deps: [HttpClient]
+          }
+        })],
+      providers: [WinLooseModalPage, NavParams, TranslateService] 
     }).compileComponents();
 
     fixture = TestBed.createComponent(WinLooseModalPage);
