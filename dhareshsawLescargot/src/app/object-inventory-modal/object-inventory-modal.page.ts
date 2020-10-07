c4782a797042d98842eca7a06a12b49215a8eb5f
E SOUSA Anabelle <anesousa@airfrance.fr>
2020-10-07 16:45:41 +0200

    feat: description display in french

--- dhareshsawLescargot/src/app/object-inventory-modal/object-inventory-modal.page.ts
+++ dhareshsawLescargot/src/app/object-inventory-modal/object-inventory-modal.page.ts
@@ -9,7 +9,8 @@ import { ObjectInventory } from '../classes/object';
   styleUrls: ['./object-inventory-modal.page.scss'],
 })
 export class ObjectInventoryModalPage implements OnInit {
-  // ---------- ATTRIBUTES ------------ //
+  
+  // ------------------------ ATTRIBUTES ------------------------ //
   modalHero: Hero;
   modalItemSelected: ObjectInventory;
   // Object to delete from the inventory list when selected by the user
@@ -17,17 +18,57 @@ export class ObjectInventoryModalPage implements OnInit {
   keyToAppear: boolean;
   // if inventory empty this boolean displays a message saying it's empty
   emptyInventoryObject: boolean;
+  // otherwise, it will fullfilled the below tab to display the bonusPower name in french
+  modalItemsDisplay: ObjectInventory[] = [];
 
-
-  // ---------- CONSTRUCTOR ------------ //
+  // ------------------------ CONSTRUCTOR ------------------------ //
   constructor(
     private modalController: ModalController,
     private navParams: NavParams
   ) {}
 
-  // -------- LIFECYCLE METHODS --------- //
+  // ------------------------ LIFECYCLE METHODS ------------------------ //
   ngOnInit() {
     this.modalHero = this.navParams.data.hero;
+
+    // if hero has empty inventory a message is displayed otherwise modalItemsDisplay is completed with
+    // the bonusPower in french
+    if (this.modalHero.items === null || this.modalHero.items.length === 0) {
+      this.emptyInventoryObject = true;
+    } else {
+      this.emptyInventoryObject = false;
+
+      // fullfilled modalItemsDisplay to display caracteristic name in french in html
+      let i: number;
+      for (i = 0; i <= this.modalHero.items.length; i++) {
+        switch (this.modalHero.items[i].bonusPower[0]) {
+          case 'endurance': {
+            this.modalItemsDisplay.push({
+              description: this.modalHero.items[i].description,
+              image: this.modalHero.items[i].image,
+              bonusPower: ['endurance', this.modalHero.items[i].bonusPower[1]],
+            });
+            break;
+          }
+          case 'strength': {
+            this.modalItemsDisplay.push({
+              description: this.modalHero.items[i].description,
+              image: this.modalHero.items[i].image,
+              bonusPower: ['force', this.modalHero.items[i].bonusPower[1]],
+            });
+            break;
+          }
+          case 'luck': {
+            this.modalItemsDisplay.push({
+              description: this.modalHero.items[i].description,
+              image: this.modalHero.items[i].image,
+              bonusPower: ['chance', this.modalHero.items[i].bonusPower[1]],
+            });
+            break;
+          }
+        }
+      }
+    }
 
     // if hero has the key, it will appear in a separate ion-card otherwise it won't appear
     if (this.modalHero.hasKey !== null) {
@@ -35,57 +76,54 @@ export class ObjectInventoryModalPage implements OnInit {
     } else {
       this.keyToAppear = false;
     }
-    
-    // if hero has empty inventory a message is displayed
-    if (this.modalHero.items === null) {
-      this.emptyInventoryObject = true;
-    } else {
-      this.emptyInventoryObject = false;
-    }
   }
 
-  // ---------- METHOD ------------ //
+  // ------------------------ METHOD ------------------------ //
 
-  // method called by the view to update the hero's Objects Inventory
+  /**
+   * method called by the view to update the hero's Objects Inventory
+   */
   updateHeroInventory(nameItemSelected: any) {
     this.modalItemSelected = this.modalHero.items.find(
       ({ description }) => description === nameItemSelected
     );
 
     switch (this.modalItemSelected.bonusPower[0]) {
-        case 'endurance': {
-          this.modalHero.endurance += this.modalItemSelected.bonusPower[1];
-          break;
-        }
-        case 'luck': {
-          this.modalHero.luck += this.modalItemSelected.bonusPower[1];
-          break;
-        }
-        case 'strength': {
-          this.modalHero.strength += this.modalItemSelected.bonusPower[1];
-          break;
-        }
+      case 'endurance': {
+        this.modalHero.endurance += this.modalItemSelected.bonusPower[1];
+        break;
+      }
+      case 'luck': {
+        this.modalHero.luck += this.modalItemSelected.bonusPower[1];
+        break;
+      }
+      case 'strength': {
+        this.modalHero.strength += this.modalItemSelected.bonusPower[1];
+        break;
+      }
     }
-    this.deleteItemFromObjectInventoryList(this.modalItemSelected.description, nameItemSelected)
-   }
+    this.deleteItemFromObjectInventoryList(nameItemSelected);
+  }
 
-  // Delete the item selected for use from the object inventory list
-  deleteItemFromObjectInventoryList(modalItemSelected: any, nameItemSelected : string) {
-     var startIndex : number; 
-     startIndex = this.modalHero.items.findIndex(
-        ({ description}) => description === nameItemSelected
-          );
-      this.modalHero.items.splice(this.modalHero[startIndex], 1 );
-      if ((this.modalHero.items == null) || (this.modalHero.items.length == 0)) {
-          this.emptyInventoryObject = true;
-          this.modalHero.items = null;
-        }
-   }
+  /**
+   * Delete the item selected for use from the object inventory list, method called by updateHeroInventory()
+   */ 
+  deleteItemFromObjectInventoryList(nameItemSelected: string) {
+    let startIndex: number;
+    startIndex = this.modalHero.items.findIndex(
+      ({ description }) => description === nameItemSelected
+    );
+    this.modalHero.items.splice(this.modalHero[startIndex], 1);
+    if (this.modalHero.items === null || this.modalHero.items.length === 0) {
+      this.emptyInventoryObject = true;
+      this.modalHero.items = null;
+    }
+  }
 
-  // method enabling to close the modal and send the pictureName to newPicturePage
+  /**
+   * method enabling to close the modal and send the pictureName to newPicturePage
+   */
   async closeObjectInventoryModal() {
-    console.log(JSON.stringify(this.modalHero));
     await this.modalController.dismiss(this.modalHero);
-
   }
 }
