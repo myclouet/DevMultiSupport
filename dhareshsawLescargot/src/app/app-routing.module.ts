ae1aaff4091b2d912ff63e23e7a64c65b4aa2ae6
LEMONNIER PAULA <AM354257@airfrance.fr>
2020-10-07 10:50:19 +0200

    bave presque ok

--- dhareshsawLescargot/src/app/app-routing.module.ts
+++ dhareshsawLescargot/src/app/app-routing.module.ts
@@ -23,10 +23,11 @@ const routes: Routes = [
   {
     path: 'history-modal',
     loadChildren: () => import('./history-modal/history-modal.module').then( m => m.HistoryModalPageModule)
-  },  {
-    path: 'win-loose-modal',
-    loadChildren: () => import('./win-loose-modal/win-loose-modal.module').then( m => m.WinLooseModalPageModule)
   },
+  //{
+  //   path: 'win-loose-modal',
+  //   loadChildren: () => import('./win-loose-modal/win-loose-modal.module').then( m => m.WinLooseModalPageModule)
+  // },
 
 ];
 
