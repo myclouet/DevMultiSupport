e96261d71109c5db5cfbf679e6655daa441902dc
Junkeesaw <kevin555j@gmail.com>
2020-10-10 00:57:34 +0200

    languageService T

--- dhareshsawLescargot/src/app/services/language.service.spec.ts
+++ dhareshsawLescargot/src/app/services/language.service.spec.ts
@@ -4,13 +4,23 @@ import { LanguageService } from './language.service';
 
 describe('LanguageService', () => {
   let service: LanguageService;
-
+  let language: any;
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(LanguageService);
+    service = new LanguageService();
   });
 
   it('should be created', () => {
+    const service: LanguageService = TestBed.get(LanguageService);
     expect(service).toBeTruthy();
+  });
+
+  it('should set and get the language', () => {
+    expect(language).toBeUndefined();
+    expect(service.getLanguage()).not.toEqual('en');
+    expect(service.getLanguage()).toEqual('fr');
+    service.setLanguage('en');
+    expect(service.getLanguage()).toEqual('en');
+    expect(service.getLanguage()).not.toEqual('fr');
   });
 });
